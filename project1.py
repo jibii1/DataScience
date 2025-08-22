eb85c99262f4de64a134b96a94d2c75edcd8db8a
jibii1 <jibinmnair101@gmail.com>
2025-08-22 20:52:15 +0530

    Initial commit

---
+++ project1.py
@@ -0,0 +1,67 @@
+# cart=["sugar", "flour", "eggs", "milk", "butter"]
+# print(cart[0])
+
+# print(cart[1:3])
+
+# cart.append("rice")
+# print(cart)
+
+# if "flour" in cart:
+# print("item present cart")
+
+
+# if "oil" in cart:
+# print("item present cart")
+
+# else:
+# print("item not present cart")
+
+
+# cart.remove("eggs")
+# print(cart)
+
+# nonveg=["chicken","beef","mutton"]
+
+# a = cart + nonveg
+# print(a)
+
+
+# cart=[{}]
+
+# while True:
+# print("1.ADD 2.VIEW 3.EXIT ")
+# choice=input("Enter your choice : ")
+    
+# if choice=="1":
+# ADD = input("Enter item to add:").strip()
+# if ADD:
+# cart.append(ADD)
+# print("added sucessfully")
+# else :
+# print("no item sepcified")
+
+# elif choice=="2":
+# for i,cart in enumerate(cart,start=1):
+# print(i,cart)
+
+# elif choice=="3":
+# print("Exicited from Cart")
+# break        
+   
+
+# cart=[]
+# n=int(input("Enter the No.of items you want to add:"))
+
+# for i in range(n):
+
+#     item=str(input("Enter the name of item :"))
+#     price=float(input("enter the price : "))
+#     quantity=str(input("enter the quantity:"))
+#     if price>5:
+#         discount=price*0.2
+#         price=price-discount
+#     n=cart.append({"item":item,"price":price,"quantity":quantity})
+#     print(cart)
+
+
+attendees = {""}
